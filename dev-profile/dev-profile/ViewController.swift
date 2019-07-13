9d778b90a8a1657a6803bc6606369cedf1584add
Shamiul Haque <samiul4413@gmail.com>
2019-07-14 03:45:10 +0600

    For Iphone

--- dev-profile/dev-profile/ViewController.swift
+++ dev-profile/dev-profile/ViewController.swift
@@ -9,10 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var profileImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //self.profileImage.layer.cornerRadius = self.profileImage.frame.size.width/2   *this is for round image*
+       // self.profileImage.clipsToBounds = true
+        self.profileImage.layer.cornerRadius = 10.0
     }
 
 
